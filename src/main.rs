303118904aa7fce2146ffab115bd24a0940ea3fe
Sudip <ghimiresdp@gmail.com>
2025-12-20 08:10:48 +0000

    update dependencies and readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("hello, furl-cli");
+use clap::Parser;
+use furl_cli::Downloader;
+use regex::Regex;
+use std::process::exit;
+
+use std::path::Path;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg()]
+    url: String,
+
+    #[arg(default_value_t = String::from("."))]
+    dest: String,
+    // TODO: add arguments for number of threads, etc.
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let path = Path::new(&args.dest);
+    if !path.exists() {
+        println!("The destination path does not exist");
+        exit(1);
+    }
+
+    // TODO: add extensive url pattern matcher
+    let re = Regex::new(r"https?://[^\s/$.?#].[^\s]*").unwrap();
+    if let Some(_) = re.captures(&args.url) {
+        let mut downloader = Downloader::new(&args.url);
+        if let Ok(_) = downloader.download(&args.dest).await {
+            println!("Download Complete!")
+        }
+    } else {
+        println!("Invalid Url parameter provided");
+        exit(1);
+    }
 }

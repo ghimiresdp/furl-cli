7fac24e0be5abf4281f28772a6290686766cdf56
Sudip <ghimiresdp@gmail.com>
2026-01-03 17:35:55 +0000

    feat: add more impl methods for config

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,6 +1,12 @@
-use std::{fs, path::PathBuf};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self, File},
+    io::{BufWriter, Error, ErrorKind},
+    path::PathBuf,
+};
+// use toml::Table;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FurlConfig {
     pub download_dir: PathBuf,
     pub threads: u8,
@@ -24,29 +30,48 @@ impl FurlConfig {
     /// however, in case it is not found, or unsupported OS, it will simply
     /// return None.
     ///
-    pub fn get_config_dir(&self) -> Option<PathBuf> {
+    pub fn get_config_dir() -> Option<PathBuf> {
         Some(dirs::config_dir()?.join(format!(
             "furl-cli/{}",
             if cfg!(debug_assertions) { "dev" } else { "" }
         )))
     }
 
-    pub fn init() -> Result<Self, std::io::Error> {
+    pub fn init() -> Result<Self, Box<dyn std::error::Error>> {
         let config = Self {
             ..FurlConfig::default()
         };
-        if let Some(config_dir) = config.get_config_dir() {
+        if let Some(config_dir) = Self::get_config_dir() {
             fs::create_dir_all(&config_dir)?;
         }
         Ok(config)
     }
     pub fn get_db_path(&self) -> Option<PathBuf> {
-        let path = self.get_config_dir()?.join("db.json");
+        let path = Self::get_config_dir()?.join("db.json");
         Some(path)
     }
 
-    pub fn get_config_file(&self) -> Option<PathBuf> {
-        let path = self.get_config_dir()?.join("config.toml");
-        Some(path)
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        if let Some(path) = Self::get_config_dir() {
+            let config_path = path.join("config.json");
+            if let Ok(config) = std::fs::read_to_string(config_path.clone()) {
+                let config = serde_json5::from_str::<FurlConfig>(&config)?;
+                return Ok(config);
+            } else {
+                let config = FurlConfig::default();
+                config.save()?;
+                return Ok(config);
+            }
+        }
+        return Err(Error::new(ErrorKind::NotFound, "Config directory not found").into());
+    }
+    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(path) = Self::get_config_dir() {
+            let file = File::create(path.join("config.json"))?;
+            let writer = BufWriter::new(file);
+            serde_json::to_writer_pretty(writer, self)?;
+            return Ok(());
+        }
+        return Err(Error::new(ErrorKind::NotFound, "Config directory not found").into());
     }
 }
